f8bc078a7075b57b9a1f29a257f4958ce43c899f
Chi <anchi@ucsd.edu>
2021-05-23 05:05:54 -0700

    demo shell script

---
+++ ec2_initialization.sh
@@ -0,0 +1,11 @@
+echo "updating apt"
+sudo apt-get update
+
+echo "downloading awscli"
+sudo apt-get awscli
+
+echo "downloading tmux"
+sudo apt-get install tmux
+
+echo "downloading python venv"
+sudo apt-get install python3-venv
