f364d412f18bdbf3bd046025fd0d3faca8c2208b
Chi <anchi@ucsd.edu>
2021-05-23 05:21:30 -0700

    changed awscli install command

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -2,7 +2,8 @@ echo "updating apt"
 sudo apt-get update
 
 echo "downloading awscli"
-sudo apt-get awscli
+sudo apt-get install awscli
+# sudo apt  install awscli
 
 echo "downloading tmux"
 sudo apt-get install tmux
