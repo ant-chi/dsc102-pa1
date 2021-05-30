b5bdb99ea69aade34d1badba6ed5090ad1ffd596
Chi <anchi@ucsd.edu>
2021-05-29 18:04:36 -0700

    add text files

--- feature_prep.py
+++ feature_prep.py
@@ -4,8 +4,11 @@ from distributed import Client
 client=Client()
 
 df1 = dd.read_csv("sample_orig_2009.txt", delimiter="|", header=None, dtype={26:"object", 28:"object"})
-df2 = dd.read_csv('sample_svcg_2009.txt', delimiter="|", header = None, dtype={26:'object', 28:'object'})
+df2 = dd.read_csv('sample_svcg_2009.txt', delimiter="|", header = None, dtype=object)
 
+
+df1.isna().mean(axis=0).compute()
+df2.isna().mean(axis=0).compute()
 
 # /home/ubuntu/env/lib/python3.8/site-packages/dask/core.py:151: DtypeWarning: Columns (3,7,23,24,28) have mixed types.Specify dtype option on import or set low_memory=False.
 #   result = _execute_task(task, cache)
